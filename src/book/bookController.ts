20c25c2a9aff6ecfd79e8416e49b105b564ee429
anikett-guptaa <aniketgt12328@gmail.com>
2025-12-08 14:30:32 +0530

    update book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -5,6 +5,7 @@ import path from 'path'
 import createHttpError from 'http-errors'
 import bookModel from './bookModel'
 import * as fs from 'node:fs';
+import { AuthRequest } from '../middlewares/authenticate'
 
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
@@ -38,7 +39,7 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     throw new Error('Book file is required')
   }
 
-  const bookFileName = files.file[0].filename // now guaranteed string
+  const bookFileName = files.file[0].filename 
 
   const bookFilePath = path.resolve(
     __dirname,
@@ -53,16 +54,11 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     format: "pdf",
   });
 
-  console.log('bookFileUploadResult',bookFileUploadResult);
-
-  console.log('UploadResult', uploadResult)
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  console.log('userId', req.userId);
+  const _req = req as AuthRequest;
   const newBook = await bookModel.create({
     title ,
     genre ,
-    author: "69300a84341cc39fea81f1a5",
+    author: _req.userId,
     coverImage: uploadResult.secure_url,
     file: bookFileUploadResult.secure_url,
   })
@@ -76,7 +72,101 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       console.log(err)
       return next(createHttpError(500,'error while uploading the files'));
 }
-
 }
 
-export { createBook }
+type MulterFiles = { [fieldname: string]: Express.Multer.File[] };
+
+const updateBook = async (req: Request, res: Response, next: NextFunction) => {
+    const { title, description, genre } = req.body;
+    const bookId = req.params.bookId;
+
+    const book = await bookModel.findOne({ _id: bookId });
+
+    if (!book) {
+        return next(createHttpError(404, "Book not found"));
+    }
+    
+    // Check access
+    const _req = req as AuthRequest;
+    if (book.author.toString() !== _req.userId) {
+        return next(createHttpError(403, "You can not update others book."));
+    }
+
+    
+    const files = (req.files as MulterFiles | undefined) || {};
+
+    //  Cover Image Upload
+    let completeCoverImage = "";
+    
+    if (files.coverImage && files.coverImage.length > 0) {
+        
+        const coverFile: Express.Multer.File = files.coverImage[0]!; 
+        
+        const filename = coverFile.filename;
+        const converMimeType = coverFile.mimetype.split("/").at(-1) ?? 'jpeg'; 
+        
+        const filePath = path.resolve(
+            __dirname,
+            "../../public/data/uploads/" + filename
+        );
+        completeCoverImage = filename;
+        
+        const uploadResult = await cloudinary.uploader.upload(filePath, {
+            filename_override: completeCoverImage,
+            folder: "book-covers",
+            format: converMimeType,
+        });
+
+        completeCoverImage = uploadResult.secure_url;
+        await fs.promises.unlink(filePath);
+    }
+
+    //  Book File Upload
+    let completeFileName = "";
+    
+    if (files.file && files.file.length > 0) {
+        
+        const bookFile: Express.Multer.File = files.file[0]!; 
+        
+        const bookFileName = bookFile.filename;
+        completeFileName = bookFileName;
+        
+        const bookFilePath = path.resolve(
+            __dirname,
+            "../../public/data/uploads/" + bookFileName
+        );
+        
+        const uploadResultPdf = await cloudinary.uploader.upload(bookFilePath, {
+            resource_type: "raw", 
+            filename_override: completeFileName,
+            folder: "book-pdfs",
+            format: "pdf",
+        });
+
+        completeFileName = uploadResultPdf.secure_url;
+        await fs.promises.unlink(bookFilePath);
+    }
+
+    // Update the Book in the Database
+    const updatedBook = await bookModel.findOneAndUpdate(
+        {
+            _id: bookId,
+        },
+        {
+            title: title,
+            description: description,
+            genre: genre,
+            coverImage: completeCoverImage
+                ? completeCoverImage
+                : book.coverImage,
+            file: completeFileName 
+                ? completeFileName 
+                : book.file,
+        },
+        { new: true }
+    );
+
+    res.json(updatedBook);
+};
+
+export { createBook,updateBook }
